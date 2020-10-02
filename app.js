7e95cdf2c852562979d6883e21f9e24a8b2c9eb3
rayannesilva <56279350+rayannesilva@users.noreply.github.com>
2020-10-02 17:39:41 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 const express = require ('express')
 const app = express()
-//const projetoRouter = require('./autorLista')
 var handlebars = require('express-handlebars');
 app.engine('handlebars',handlebars({defaultLayout:'mainLayout'}))
 app.set('view engine', 'handlebars') 
-//autor = [{ nome:"habitantes: 29"  },{nome:"habitantes: 39" },{nome:"habitantes: 1000000"}]
 //questao um -- item um e dois -- com view
-app.get('/autor/lista', (req,res) =>{
-	
+app.get('/autor/listas', (req,res) =>{
 	res.render('autorLista', {autores : [{ nome:"bloch"  },{nome:"hobsbawn" },{nome:"thompson"}]})
 
 })
